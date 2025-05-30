ddd19ead70aae3adb323a3fca42b20f1eb46a6fc
Alejandro Matías Astorga <alejandromatiasastorga@gmail.com>
2025-05-30 17:27:00 -0600

    fix: fixed src route to recognize the right module used

--- tests/test_collaborative_filtering.py
+++ tests/test_collaborative_filtering.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add src/ to the path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.collaborative_filtering import CollaborativeFilteringRecommender
 
 
